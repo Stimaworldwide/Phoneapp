1f21f8da02941ae178ee600952ba0cee1a57e6b4
Tafadzwa Stima <71855040+Stimaworldwide@users.noreply.github.com>
2021-12-20 07:08:33 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace phoneapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Contact c = new Contact();
+            c.name = txtName.Text;
+            c.number = txtnum.Text;
+            string next = Serialization.serialize(c);
+            File.WriteAllText("C:\\PHONEAPP\\Sereal.xml", next);
+            MessageBox.Show("Saved");
+            
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Serialization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace phoneapp
+{
+    public class Serialization
+    {
+        public static string serialize(Contact c)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(typeof(Contact));
+            string xml = "";
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, c);
+                    xml = sww.ToString();//my xml
+                }
+            }
+            return xml;
+        }
+
+    }
+}
